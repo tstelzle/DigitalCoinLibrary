4625cae547fd98c5e67ae2d5ee8d577cdaa08b36
Tim Mewe <tim.mewe@jambit.com>
2022-11-29 19:39:19 +0100

    Move coin url to constants file

---
+++ flutter_frontend/lib/core/constants.dart
@@ -0,0 +1,2 @@
+const String apiBaseUrl = "http://192.168.177.21:9010/api";
+const String coinUrl = "$apiBaseUrl/coin";
--- flutter_frontend/lib/home/coin_list.dart
+++ flutter_frontend/lib/home/coin_list.dart
@@ -5,6 +5,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
 import '../model/coin.dart';
+import '../core/constants.dart' as Constants;
 
 class CoinList extends ChangeNotifier {
   Map<String, List<Coin>> coinMap = {};
@@ -14,14 +15,16 @@ class CoinList extends ChangeNotifier {
   }
 
   Future<void> updateMap() async {
-    final response = await http.get(Uri.parse("http://192.168.177.21:9010/api/coin"));
-    if (response.statusCode == HttpStatus.ok) {
-      Map countryMap = Map.from(jsonDecode(utf8.decode(response.bodyBytes)));
-
-      countryMap.forEach((key, value) {
-        coinMap.putIfAbsent(key, () => List<Coin>.from(value.map((coin) => Coin.fromJson(coin))));
-      });
+    final response = await http.get(Uri.parse(Constants.coinUrl));
+    if (response.statusCode != HttpStatus.ok) {
+      return;
     }
+
+    Map countryMap = Map.from(jsonDecode(utf8.decode(response.bodyBytes)));
+
+    countryMap.forEach((key, value) {
+      coinMap.putIfAbsent(key, () => List<Coin>.from(value.map((coin) => Coin.fromJson(coin))));
+    });
 
     notifyListeners();
   }
