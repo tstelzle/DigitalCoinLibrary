a333cb803c43fdb244e73d1fdc646c780ad9f663
tstelzle <tarek.stelzle@gmx.de>
2023-05-16 22:02:20 +0200

    Added FrontImageController;

---
+++ Backend/src/main/java/com/coinlibrary/backend/controller/FrontImageController.java
@@ -0,0 +1,32 @@
+package com.coinlibrary.backend.controller;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+import java.io.InputStream;
+
+@WebFilter(urlPatterns = "/*")
+@RestController
+@RequestMapping
+public class FrontImageController {
+
+    @GetMapping("/api/frontImage/{name}")
+    public ResponseEntity<byte[]> getImage(@PathVariable String name) throws IOException {
+        InputStream in = getClass().getResourceAsStream("/coin_fronts/" + name + ".png");
+        if (in == null) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        byte[] image = IOUtils.toByteArray(in);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_JPEG);
+        headers.setContentLength(image.length);
+        return new ResponseEntity<>(image, headers, HttpStatus.OK);
+    }
+}
