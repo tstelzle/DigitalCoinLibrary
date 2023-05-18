738d16339d834f2d0c8096c37687074adc7f8b55
tstelzle <tarek.stelzle@gmx.de>
2023-05-18 10:32:03 +0200

    Using HTTPS Without Port;

--- flutter_frontend/lib/core/constants.dart
+++ flutter_frontend/lib/core/constants.dart
@@ -1,4 +1,4 @@
 const String ipAddress = String.fromEnvironment("API_URL");
 const String port = String.fromEnvironment("API_PORT");
-final String apiBaseUrl = port.isEmpty ? "http://$ipAddress/api" : "http://$ipAddress:$port/api";
+final String apiBaseUrl = port.isEmpty ? "https://$ipAddress/api" : "http://$ipAddress:$port/api";
 final String coinUrl = "$apiBaseUrl/coin";
