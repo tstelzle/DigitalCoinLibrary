4d7b9ecf65f2b0c90f5b3365b1cbbb50867590c5
tstelzle <tarek.stelzle@gmx.de>
2023-05-22 15:31:08 +0200

    Added Const Modifier;

--- flutter_frontend/lib/home/home_page.dart
+++ flutter_frontend/lib/home/home_page.dart
@@ -28,7 +28,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 (BuildContext context, AsyncSnapshot<List<Edition>> snapshot) {
               if (snapshot.hasData) {
                 return ListView.builder(
-                    physics: NeverScrollableScrollPhysics(),
+                    physics: const NeverScrollableScrollPhysics(),
                     shrinkWrap: true,
                     itemCount: snapshot.data?.length,
                     itemBuilder: (BuildContext context, int index) {
