5b97575335ab29d5af166b306bfee50412272f6c
tstelzle <tarek.stelzle@gmx.de>
2023-05-22 16:35:33 +0200

    Add Scaling PoPup for CoinCard;

--- flutter_frontend/lib/home/coin_card.dart
+++ flutter_frontend/lib/home/coin_card.dart
@@ -18,34 +18,49 @@ class _CoinCardState extends State<CoinCard> {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {
-        setState(() {
-          showBack = !showBack;
-        });
-      },
-      child: Card(
-          color: widget.coin.available ? Colors.green : Colors.red,
-          child: showBack
-              ? widget.coin.imagePath.isEmpty
-                  ? const FittedBox(child: Icon(Icons.do_disturb))
-                  : ClipOval(
-                      child: Image.network(widget.coin.imagePath,
-                          fit: BoxFit.cover, errorBuilder:
-                              (BuildContext context, Object exception,
-                                  StackTrace? stackTrace) {
-                      return networkError();
-                    }))
-              : Image.network(
-                  generateUri("$frontImage${widget.coin.coinSize}", {})
-                      .toString(),
-                  fit: BoxFit.cover, errorBuilder: (BuildContext context,
-                      Object exception, StackTrace? stackTrace) {
-                  return networkError();
-                })),
-    );
+        onTap: () {
+          setState(() {
+            showBack = !showBack;
+          });
+        },
+        onDoubleTap: _showImagePopup,
+        child: getCard());
+  }
+
+  Widget getCard() {
+    return Card(
+        color: widget.coin.available ? Colors.green : Colors.red,
+        child: showBack
+            ? widget.coin.imagePath.isEmpty
+                ? const FittedBox(child: Icon(Icons.do_disturb))
+                : ClipOval(
+                    child: Image.network(widget.coin.imagePath,
+                        fit: BoxFit.cover, errorBuilder: (BuildContext context,
+                            Object exception, StackTrace? stackTrace) {
+                    return networkError();
+                  }))
+            : Image.network(
+                generateUri("$frontImage${widget.coin.coinSize}", {})
+                    .toString(),
+                fit: BoxFit.cover, errorBuilder: (BuildContext context,
+                    Object exception, StackTrace? stackTrace) {
+                return networkError();
+              }));
   }
 
   Widget networkError() {
     return const FittedBox(child: Icon(Icons.wifi_off));
+  }
+
+  void _showImagePopup() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          contentPadding: EdgeInsets.zero,
+          content: getCard(),
+        );
+      },
+    );
   }
 }
