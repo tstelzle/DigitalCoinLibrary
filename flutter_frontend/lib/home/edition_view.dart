c01668fdb2ffc134296ff43fb306952a5e9dcd6b
tstelzle <tarek.stelzle@gmx.de>
2023-07-11 19:28:31 +0200

    Not Displaying Empty Editions;

--- flutter_frontend/lib/home/edition_view.dart
+++ flutter_frontend/lib/home/edition_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_frontend/core/coin_api.dart';
 
+import '../core/filter_state.dart';
 import '../model/coin.dart';
 import '../model/edition.dart';
 import 'coin_card.dart';
@@ -22,40 +24,46 @@ class _EditionViewState extends State<EditionView>
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      ListTile(title: Text(widget.edition.editionString)),
-      LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-          final screenWidth = constraints.maxWidth;
-          const itemWidth =
-              100.0; // Adjust this value based on your item's desired width
-          final crossAxisCount = (screenWidth / itemWidth).floor();
-
-          return FutureBuilder<List<Coin>>(
-            future: coinApi.fetchCoinsByEdition(widget.edition.id),
+    return BlocBuilder<FilterBloc, FilterState>(builder: (context, state) {
+      return LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+        final screenWidth = constraints.maxWidth;
+        const itemWidth =
+            100.0; // Adjust this value based on your item's desired width
+        final crossAxisCount = (screenWidth / itemWidth).floor();
+        return FutureBuilder<List<Coin>>(
+            future:
+                coinApi.fetchCoinsByEdition(widget.edition.id, state.coinSize),
             builder:
                 (BuildContext context, AsyncSnapshot<List<Coin>> snapshot) {
               if (snapshot.hasData) {
-                return GridView.builder(
-                  physics: const ScrollPhysics(),
-                  shrinkWrap: true,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: crossAxisCount,
-                    crossAxisSpacing: 5.0,
-                    mainAxisSpacing: 5.0,
-                  ),
-                  itemCount: snapshot.data?.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return CoinCard(coin: snapshot.data![index]);
-                  },
-                );
+                if (snapshot.data?.length != 0) {
+                  return Column(children: <Widget>[
+                    ListTile(title: Text(widget.edition.editionString)),
+                    GridView.builder(
+                      physics: const ScrollPhysics(),
+                      shrinkWrap: true,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: crossAxisCount,
+                        crossAxisSpacing: 5.0,
+                        mainAxisSpacing: 5.0,
+                      ),
+                      itemCount: snapshot.data?.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return CoinCard(coin: snapshot.data![index]);
+                      },
+                    )
+                  ]);
+                } else {
+                  return const SizedBox();
+                }
               } else {
-                return const CircularProgressIndicator(color: Colors.black);
+                return Column(children: const <Widget>[
+                  CircularProgressIndicator(color: Colors.black)
+                ]);
               }
-            },
-          );
-        },
-      ),
-    ]);
+            });
+      });
+    });
   }
 }
