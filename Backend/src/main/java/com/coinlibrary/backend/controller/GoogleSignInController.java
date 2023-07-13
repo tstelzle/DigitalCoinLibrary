5cbdd43347cedb4ec8a9866fe0800e449dc55ffd
tstelzle <tarek.stelzle@gmx.de>
2023-07-13 10:48:52 +0200

    GoogleTokenVerifier;

--- Backend/src/main/java/com/coinlibrary/backend/controller/GoogleSignInController.java
+++ Backend/src/main/java/com/coinlibrary/backend/controller/GoogleSignInController.java
@@ -1,45 +1,29 @@
-//package com.coinlibrary.backend.controller;
-//
-//import com.coinlibrary.backend.service.GoogleSignInService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping
-//public class GoogleSignInController {
-//
-//    private final GoogleSignInService googleSignInService;
-//
-//    @Autowired
-//    public GoogleSignInController(GoogleSignInService googleSignInService) {
-//        this.googleSignInService = googleSignInService;
-//    }
-//
-//
-//    @GetMapping("/api/authenticate")
-//    public ResponseEntity<?> signInWithGoogle(@RequestParam String idToken) {
-//        return new ResponseEntity<>(googleSignInService.validGoogleUser(idToken), HttpStatus.OK);
-//    }
-//}
+package com.coinlibrary.backend.controller;
 
-//TODO breaks selenium
-//<dependency>
-//<groupId>com.google.api-client</groupId>
-//<artifactId>google-api-client</artifactId>
-//<version>1.31.1</version>
-//</dependency>
-//<dependency>
-//<groupId>com.fasterxml.jackson.core</groupId>
-//<artifactId>jackson-core</artifactId>
-//<version>2.12.4</version>
-//</dependency>
-//<dependency>
-//<groupId>com.fasterxml.jackson.core</groupId>
-//<artifactId>jackson-databind</artifactId>
-//<version>2.12.4</version>
-//</dependency>
+import com.coinlibrary.backend.service.GoogleTokenVerifier;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class GoogleSignInController {
+
+    private final GoogleTokenVerifier googleTokenVerifier;
+
+    @Autowired
+    public GoogleSignInController(GoogleTokenVerifier googleTokenVerifier) {
+        this.googleTokenVerifier = googleTokenVerifier;
+    }
+
+
+    @GetMapping("/api/authenticate")
+    public ResponseEntity<?> signInWithGoogle(@RequestParam String idToken) {
+        return new ResponseEntity<>(googleTokenVerifier.verify(idToken), HttpStatus.OK);
+        // TODO create user if verify returned true
+    }
+}
---
+++ Backend/src/main/java/com/coinlibrary/backend/service/GoogleTokenVerifier.java
@@ -0,0 +1,69 @@
+package com.coinlibrary.backend.service;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+
+@Service
+@Slf4j
+public class GoogleTokenVerifier {
+
+    private final NetHttpTransport transport;
+    private final JsonFactory jsonFactory;
+    @Value("${google.client.id}")
+    private String googleClientId;
+
+    public GoogleTokenVerifier() {
+        this.transport = new NetHttpTransport();
+        this.jsonFactory = GsonFactory.getDefaultInstance();
+    }
+
+    public boolean verify(String idTokenString) {
+        try {
+            GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(transport, jsonFactory)
+                    .setAudience(Collections.singletonList(googleClientId))
+                    // Or, if multiple clients access the backend:
+                    //.setAudience(Arrays.asList(CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3))
+                    .build();
+
+            GoogleIdToken idToken = verifier.verify(idTokenString);
+            if (idToken != null) {
+                Payload payload = idToken.getPayload();
+
+                // Print user identifier
+                String userId = payload.getSubject();
+                System.out.println("User ID: " + userId);
+
+                // Get profile information from payload
+                String email = payload.getEmail();
+                boolean emailVerified = payload.getEmailVerified();
+                String name = (String) payload.get("name");
+                String pictureUrl = (String) payload.get("picture");
+                String locale = (String) payload.get("locale");
+                String familyName = (String) payload.get("family_name");
+                String givenName = (String) payload.get("given_name");
+
+                return true;
+
+                // Use or store profile information
+                // ...
+
+            } else {
+                System.out.println("Invalid ID token.");
+                return false;
+            }
+        } catch (GeneralSecurityException | IOException exception) {
+            return false;
+        }
+    }
+}
