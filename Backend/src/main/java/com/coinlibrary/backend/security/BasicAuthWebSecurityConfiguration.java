91ed07f42a7f57df60e01f22330c7d49d3cc5f65
tstelzle <tarek.stelzle@gmx.de>
2023-11-28 16:12:17 +0100

    New Cors Config;

--- Backend/src/main/java/com/coinlibrary/backend/security/BasicAuthWebSecurityConfiguration.java
+++ Backend/src/main/java/com/coinlibrary/backend/security/BasicAuthWebSecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,6 +17,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class BasicAuthWebSecurityConfiguration {
 
     @Value("${app.username}")
@@ -26,7 +28,8 @@ public class BasicAuthWebSecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http.cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.GET)
                         .permitAll()
                         .requestMatchers(HttpMethod.OPTIONS)
