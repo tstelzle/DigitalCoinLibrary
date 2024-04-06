3b0304bdb2d7f76063157ba12d1d6cf70acc07b7
tstelzle <tarek.stelzle@gmx.de>
2024-04-06 21:54:00 +0200

    fix: Removed Unused Properties In Spring Configuration;

--- Backend/src/main/java/com/coinlibrary/backend/BasicAuthWebSecurityConfiguration.java
+++ Backend/src/main/java/com/coinlibrary/backend/BasicAuthWebSecurityConfiguration.java
@@ -5,10 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -26,8 +24,6 @@ import java.util.Arrays;
 @EnableWebSecurity(debug = true)
 public class BasicAuthWebSecurityConfiguration {
 
-    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
-    String jwkSetUri;
     @Value("${app.frontend.url}")
     private String frontendURL;
     @Value("${app.backend.username}")
