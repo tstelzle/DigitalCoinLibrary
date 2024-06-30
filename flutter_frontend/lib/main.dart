9e4d44cea7cd49ab5301d0a48a10b4d138b79b94
tstelzle <tarek.stelzle@gmx.de>
2024-06-30 13:03:16 +0200

    Revert "Instantiated Google Sign Plugin Web"
    This reverts commit b2cc095e7ebf875bed81e4436e2a3ea9096858ec.

--- flutter_frontend/lib/main.dart
+++ flutter_frontend/lib/main.dart
@@ -3,11 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_frontend/core/filter_state.dart';
 import 'package:flutter_frontend/core/user_state.dart';
 import 'package:flutter_frontend/home/home_page.dart';
-import 'package:google_sign_in_platform_interface/google_sign_in_platform_interface.dart';
-import 'package:google_sign_in_web/google_sign_in_web.dart' as web;
 
 void main() {
-  GoogleSignInPlatform.instance = web.GoogleSignInPlugin();
   runApp(
     MultiBlocProvider(
       providers: [
