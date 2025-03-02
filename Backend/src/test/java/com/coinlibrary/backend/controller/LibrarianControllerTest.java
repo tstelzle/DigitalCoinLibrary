ba44caa6a7d6820e04284b376fd2c3468e58676b
tstelzle <tarek.stelzle@gmx.de>
2025-03-02 12:43:53 +0100

    test: wip integrationtest

---
+++ Backend/src/test/java/com/coinlibrary/backend/controller/LibrarianControllerTest.java
@@ -0,0 +1,24 @@
+package com.coinlibrary.backend.controller;
+
+import org.apache.catalina.connector.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibrarianControllerTest {
+
+/*    @Test
+    public void givenUserWithReadScope_whenGetFooResource_thenSuccess() {
+        String accessToken = obtainAccessToken("read");
+
+        Response response = RestAssured.given()
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                .get("http://localhost:8081/resource-server-jwt/foos");
+        assertThat(response.as(List.class)).hasSizeGreaterThan(0);
+    }*/
+
+}
