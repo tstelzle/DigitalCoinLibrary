1d4d8dfdebf7eee70c6f1f0f8958905c3776423d
tstelzle <tarek.stelzle@gmx.de>
2025-06-30 20:55:08 +0200

    feat: Remove LayoutBuilder let GridViewBuilder figure out the width;

--- flutter_frontend/lib/view/edition_view.dart
+++ flutter_frontend/lib/view/edition_view.dart
@@ -9,31 +9,24 @@ class EditionView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (BuildContext context, BoxConstraints constraints) {
-        final screenWidth = constraints.maxWidth;
-        const itemWidth =
-            100.0; // Adjust this value based on your item's desired width
-        final crossAxisCount = (screenWidth / itemWidth).floor();
-        return Column(
-          children: <Widget>[
-            ListTile(title: Text(edition.editionString)),
-            GridView.builder(
-              physics: const ScrollPhysics(),
-              shrinkWrap: true,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: crossAxisCount,
-                crossAxisSpacing: 5,
-                mainAxisSpacing: 5,
-              ),
-              itemCount: edition.coins.length,
-              itemBuilder: (BuildContext context, int index) {
-                return CoinView(coin: edition.coins[index]);
-              },
-            ),
-          ],
-        );
-      },
+    return Column(
+      children: <Widget>[
+        ListTile(title: Text(edition.editionString)),
+        GridView.builder(
+          physics: const ScrollPhysics(),
+          shrinkWrap: true,
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 100,
+            mainAxisExtent: 100,
+            crossAxisSpacing: 5,
+            mainAxisSpacing: 5,
+          ),
+          itemCount: edition.coins.length,
+          itemBuilder: (BuildContext context, int index) {
+            return CoinView(coin: edition.coins[index]);
+          },
+        ),
+      ],
     );
   }
 }
