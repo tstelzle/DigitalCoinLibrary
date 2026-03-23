52a18db2c3af916f3ccb8143624bff4d87e1dc48
tstelzle <tarek.stelzle@gmx.de>
2026-03-23 22:42:40 +0100

    feat (Frontend): Include url for ecb in imprint;

--- flutter_frontend/lib/ImprintPage/imprint_page.dart
+++ flutter_frontend/lib/ImprintPage/imprint_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ImprintPage extends StatelessWidget {
   const ImprintPage({Key? key}) : super(key: key);
@@ -12,24 +13,37 @@ class ImprintPage extends StatelessWidget {
           style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
         ),
       ),
-      body: const SingleChildScrollView(
-        padding: EdgeInsets.all(16),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
+            const Text(
               'Kontakinformationen',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-            Text('Tarek Stelzle'),
-            Text('Karl-Frowein-Str. 21, 53115 Bonn'),
-            Text('tarek.stelzle@gmx.de'),
-            Text(
+            const Text('Tarek Stelzle'),
+            const Text('Karl-Frowein-Str. 21, 53115 Bonn'),
+            const Text('tarek.stelzle@gmx.de'),
+            const Text(
               'Quellenangabe für verwendete Bilder & Grafiken',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-            Text(
-                'Das von uns verwendete Bildmaterial unterliegt dem Copyright der jeweiligen Inhaber und Fotografen. Dies ist die Europäische Zentralbank.'),
+            const Text(
+              'Das von uns verwendete Bildmaterial unterliegt dem Copyright der jeweiligen Inhaber und Fotografen. Dies ist die Europäische Zentralbank.',
+            ),
+            TextButton(
+              onPressed: () async => {
+                await launchUrl(
+                  Uri.parse(
+                    'https://www.ecb.europa.eu/euro/coins/html/index.en.html',
+                  ),
+                )
+              },
+              child: const Text(
+                'https://www.ecb.europa.eu/euro/coins/html/index.en.html',
+              ),
+            ),
           ],
         ),
       ),
